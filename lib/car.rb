f7452d5eed54cd9ca464d6d9e6d0044f67a029ba
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-08 09:41:27 -0700

    Create total_cost method.

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,5 @@
 class Car
-
+attr_reader :monthly_payment, :loan_length 
   def initialize(make_and_model, monthly_payment, loan_length)
     @make_and_model = make_and_model
     @monthly_payment = monthly_payment
@@ -8,5 +8,13 @@ class Car
 
   def make
     @make_and_model.split[0]
+  end
+
+  def model
+    @make_and_model.split[1]
+  end
+
+  def total_cost
+    (@loan_length * @monthly_payment)
   end
 end
