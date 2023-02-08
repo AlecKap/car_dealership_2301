9ec0468458542442ac83d98de2eda3fe55a7ab35
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-08 09:45:51 -0700

    Add color instance variable to car class.

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,10 @@
 class Car
-attr_reader :monthly_payment, :loan_length 
+attr_reader :monthly_payment, :loan_length, :color
   def initialize(make_and_model, monthly_payment, loan_length)
     @make_and_model = make_and_model
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+    @color = nil
   end
 
   def make
