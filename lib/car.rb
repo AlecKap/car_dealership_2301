2c3655eb8d5d2200c49ded6c9be15e88ede96d66
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-08 09:25:17 -0700

    Initial Commit

---
+++ lib/car.rb
@@ -0,0 +1,7 @@
+class Car
+  def initialize(make_and_model, monthly_payment, loan_length)
+    @make_and_model = make_and_model
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+  end
+end
