98e14185bb8b55950b2cf9d19db3f8fdb9f17b7b
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-08 10:24:41 -0700

    Create test for has_inevtory? method.

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe 'Dealership' do
     
     expect(dealership.inventory_count).to be 0
   end
+
+  it 'adds cars to inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Streat")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.inventory).to eq([car_1, car_2])
+    expect(dealership.inventory_count).to be 2
+  end
+
+  it 'has inventory?' do
+    dealership = Dealership.new("Acme Auto", "123 Main Streat")
+    
+    expect(dealership.has_inventory?).to be false
+  end
+
 end
