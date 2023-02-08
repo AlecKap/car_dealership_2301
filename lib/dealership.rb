f41ffcd2e800fdc333b223d5c76a699f600bcccb
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-08 10:05:43 -0700

    Add instance variable to Dealership class.

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,8 +1,10 @@
 require './lib/car'
 
 class Dealership
+  attr_reader :name, :location, :inventory
   def initialize(name, location)
     @name = name
     @location = location
+    @inventory = []
   end
 end
